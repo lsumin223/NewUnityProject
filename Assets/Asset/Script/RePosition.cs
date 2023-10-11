ca366545f1d36dda89e861d403e581a83c5ec62e
hyeok <zhzh4040@naver.com>
2023-10-11 11:06:25 +0900

    무한맵

---
+++ Assets/Asset/Script/RePosition.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+
+        float diffX = playerPos.x - myPos.x;
+        float diffY = playerPos.y - myPos.y;
+        float dirX = Mathf.Abs(diffX)/28 > Mathf.Abs(diffY)/18 ? Mathf.Sign(diffX) : 0;
+
+        float dirY = Mathf.Abs(diffY)/18 > Mathf.Abs(diffX)/28 ? Mathf.Sign(diffY) : 0;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (dirX != 0)
+                {
+                    transform.Translate(Vector3.right * dirX * 56);
+                }
+                else if (dirY != 0)
+                {
+                    transform.Translate(Vector3.up * dirY * 36);
+                }
+                break;
+        }
+    }
+}
